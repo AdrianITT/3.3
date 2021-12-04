6907def93fd5c0c964815371392fe17004e74022
Adrian Noe Alvarez Zacarias <80360127+AdrianITT@users.noreply.github.com>
2021-12-03 21:04:30 -0800

    Create eje4.c

---
+++ eje4.c
@@ -0,0 +1,9 @@
+# include < stdio .h >
+void main ( void ){
+int i , suma ;
+int vector [5]= {128 , 32 , 100 , -30 , 124};
+for ( suma = i = 0; i <5; i ++ ){
+  suma += vector [i ];
+}
+printf (" La suma es %d \n" , suma );
+}
